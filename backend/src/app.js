6cdacfc1dbfeca2aa7dd9270cf009e1f29f09b1d
Baweke Mekonen <Baweke.Mekonen@nibbank.com.et>
2026-06-29 16:57:02 +0300

    cors change

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:5173', 'https://shager-wedding.onrender.com'],
   credentials: true
 }));
 
